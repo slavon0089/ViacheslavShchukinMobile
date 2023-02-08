d045a9f73e4068881bd6d0dfa985a1c88de246ea
Viacheslav_Shchukin <slavon0089@yandex.ru>
2023-02-08 16:00:00 +0100

    native iOS

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -3,42 +3,50 @@ package pageObjects;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class NativePageObject  {
-
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@value='Register new account']")
+    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
+    WebElement registerNewAccountBtn;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@label='Sign In']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
     WebElement signInBtn;
-
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@value='user@example.com']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
     WebElement emailField;
-
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField[@value='Required']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
     WebElement passwordField;
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/decor_content_parent")
     WebElement mainPageLogo;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeOther[@name='Budget']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/expenses_list")
     WebElement budgetActivityList;
 
-    @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
-    WebElement registerNewAccountBtn;
-
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@value='user@example.com']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
     WebElement emailRegField;
-
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@value='TimApple']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
     WebElement userRegField;
-
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField[@value='Required']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
     WebElement passwordRegField;
-
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField[@value='Repeat please']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
     WebElement confirmPasswordRegField;
 
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSwitch[@value='0']")
+    WebElement agreamentsBtn;
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@label='Register new account']")
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
     WebElement confirmRegBtn;
+
+
 
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements( new AppiumFieldDecorator(appiumDriver), this);
--- src/test/java/scenarios/LoginAndRegNativeMobileTests.java
+++ src/test/java/scenarios/LoginAndRegNativeMobileTests.java
@@ -3,6 +3,7 @@ package scenarios;
 import data.InputData;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import setup.BaseTest;
 
@@ -10,21 +11,28 @@ import static data.InputData.*;
 
 public class LoginAndRegNativeMobileTests extends BaseTest {
 
+    @Parameters("platformName")
     @Test(groups = {"Registration"}, description = "The  log in test",priority = 1)
-    public void registrationNativeTest() throws IllegalAccessException, NoSuchFieldException, InstantiationException {
+    public void registrationNativeTest(String platformName) throws IllegalAccessException, NoSuchFieldException, InstantiationException {
         getPo().getWelement("registerNewAccountBtn").click();
         getPo().getWelement("emailRegField").sendKeys(userName);
         getPo().getWelement("userRegField").sendKeys(userName);
         getPo().getWelement("passwordRegField").sendKeys(password);
         getPo().getWelement("confirmPasswordRegField").sendKeys(password);
+        if(platformName=="iOS"){
+            getPo().getWelement("agreamentsBtn").click();
+            getPo().getWelement("confirmRegBtn").click();
+        }
+
         getPo().getWelement("confirmRegBtn").click();
+
         Assert.assertTrue(getPo().getWelement("signInBtn").isDisplayed());
 
     }
     @Test(groups = {"Registration"}, description = "The  log in test",priority = 2)
     public void loginNativeTest()throws IllegalAccessException, NoSuchFieldException, InstantiationException{
         getPo().getWelement("emailField").sendKeys(userName);
-        getPo().getWelement("passwordField").sendKeys(userName);
+        getPo().getWelement("passwordField").sendKeys(password);
         getPo().getWelement("signInBtn").click();
         Assert.assertTrue(getPo().getWelement("budgetActivityList").isDisplayed());
     }
